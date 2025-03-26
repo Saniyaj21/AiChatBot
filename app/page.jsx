3335a5bf7eb9fbf5b057fffce8db13dc23d509a6
saniyaj Mallik <saniyaj.mallik@wisdmlabs.com>
2025-03-26 19:57:35 +0530

    INIT

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-center bg-blue-600 p-4 text-5xl'>
+      hello
+    </div>
+  )
+}
+
+export default page
---
+++ app/products/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '@/styles/productsPage.css'
+
+const page = () => {
+  return (
+    <div className='p'>
+      products
+    </div>
+  )
+}
+
+export default page
