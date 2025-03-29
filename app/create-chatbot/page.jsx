76bbaed20538ae62481e673ae42e3306dd1aef28
saniyaj Mallik <saniyaj.mallik@wisdmlabs.com>
2025-03-29 20:45:44 +0530

    2nd day code with Auth

--- app/create-chatbot/page.jsx
+++ app/create-chatbot/page.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import '@/styles/productsPage.css'
 
 const page = () => {
   return (
-    <div className='p'>
-      products
+    <div>
+      Create Chat bot
     </div>
   )
 }
